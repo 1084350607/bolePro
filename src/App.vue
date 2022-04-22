29e0e1ab1e164ee18ded1adc0fd9e39e88867714
1084350607 <1084350607>
2022-04-22 20:41:52 +0800

    update some viteconfig

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from '@/comp/HelloWorld.vue'
+import HelloWorld from 'comp/HelloWorld.vue'
 </script>
 
 <template>
