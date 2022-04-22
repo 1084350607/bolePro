4e575407800aa43b0a208f0e2c3b01b1fa553555
1084350607 <1084350607>
2022-04-22 19:49:06 +0800

    add some alias

--- bolePro/vite.config.ts
+++ bolePro/vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  resolve: {
+    alias: {
+      "@/src": resolve(__dirname, 'src'),
+      "@/comp": resolve(__dirname, './src/components'),
+      '@/assets': resolve(__dirname, './src/assets'),
+      '@/public': resolve(__dirname, './public')
+    }
+  },
   plugins: [vue()]
 })
